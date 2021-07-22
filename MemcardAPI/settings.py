751889b34ddfc4e06a482025048833509b33ff6d
sblip <65819451+shaurya-blip@users.noreply.github.com>
2021-07-22 11:05:39 +0530

    yes

--- MemcardAPI/settings.py
+++ MemcardAPI/settings.py
@@ -142,7 +142,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.BasicAuthentication'
     ),
