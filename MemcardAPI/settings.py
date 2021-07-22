4418acdccd42542f2aa0187f4080ff044c9bf3ae
sblip <65819451+shaurya-blip@users.noreply.github.com>
2021-07-22 11:23:05 +0530

    lolepopop

--- MemcardAPI/settings.py
+++ MemcardAPI/settings.py
@@ -142,10 +142,10 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication'
+        'rest_framework.authentication.BasicAuthentication'
     ),
 }
 AUTHENTICATION_BACKENDS = (
