519b622ce74df60a9a244e2efeeaf312b1140849
sblip <65819451+shaurya-blip@users.noreply.github.com>
2021-07-22 10:59:19 +0530

    lollipop

--- MemcardAPI/settings.py
+++ MemcardAPI/settings.py
@@ -141,11 +141,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication'
-    ],
+    ),
 }
 AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
