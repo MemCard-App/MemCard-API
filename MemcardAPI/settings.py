d54013504e2b586cecab49a73f987e2001539621
sblip <65819451+shaurya-blip@users.noreply.github.com>
2021-07-22 10:40:40 +0530

    added session auth again

--- MemcardAPI/settings.py
+++ MemcardAPI/settings.py
@@ -143,7 +143,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication'
     ),
 }
 AUTHENTICATION_BACKENDS = (
